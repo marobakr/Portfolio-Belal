acbc00a0b4530a5606541c780585e6489745fadd
marwanabubakr <dev@marobakr>
2024-03-23 14:44:17 +0200

    home section

---
+++ Portfolio-Belal/src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HeroComponent } from './core/components/hero/hero.component';
+
+@NgModule({
+  declarations: [AppComponent, HeroComponent],
+  imports: [BrowserModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
